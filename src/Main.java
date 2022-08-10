e102025ec7ed4e69a41c529c2a94bdef4162807e
yanap <yanapenner96@gmail.com>
2022-08-10 12:58:17 +0300

    выполнено 4 задания

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        // Задача 1
         byte a = 120;
         short b = 324;
         int c = 3_456_234;
@@ -17,5 +18,41 @@ public class Main {
         System.out.println("переменная шестого типа равна " +f);
         System.out.println("переменная седьмого типа равна " +g);
         System.out.println("переменная восьмого типа равна " +h);
+
+        // Задача 2
+        float boxer1Weight = 78.2f;
+        float boxer2Weight = 82.7f;
+        System.out.println("Общий вес двух боксеров равен " + (boxer1Weight + boxer2Weight) + " кг");
+        System.out.println("Разница между весами боксеров равна " + Math.abs(boxer1Weight - boxer2Weight) + " кг");
+
+        // Задача 3
+        byte banana = 5;
+        short bananaWeight = 80;
+        short milk = 200;
+        short milkWeight = 105;
+        byte iceCream = 2;
+        short iceCreamWeight = 100;
+        byte egg = 4;
+        byte eggWeight = 70;
+        int totalWeight = banana*bananaWeight+milk*milkWeight+iceCream*iceCreamWeight+egg*eggWeight;
+        System.out.println("Вес спорт-завтрака равен " + totalWeight + " грамм");
+        float totalWeightK = totalWeight/1_000f;
+        System.out.println("Вес спорт-завтрака равен в кг " + totalWeightK);
+
+        // Задание 4
+        int lossWeight = 7*1_000;
+        short lossOneDayWeight1 = 250;
+        short lossOneDayWeight2 = 500;
+        int daysLossWeight1 = lossWeight/lossOneDayWeight1;
+        int daysLossWeight2 = lossWeight/lossOneDayWeight2;
+        int averageLossOneDayWeight = (lossOneDayWeight1+lossOneDayWeight2)/2;
+        int averageDaysLossWeight = lossWeight/averageLossOneDayWeight;
+        System.out.println("Если каждый день терять по 250 гр, то на похудение уйдет "+daysLossWeight1+" дней");
+        System.out.println("Если каждый день терять по 500 гр, то на похудение уйдет "+daysLossWeight2+" дней");
+        System.out.println("В среднем на похудение уйдет "+averageDaysLossWeight+" дней");
+
+
+
+
     }
 }
