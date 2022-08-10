60403b956e8da3f24ddc0fc893c5d9478ee49321
yanap <yanapenner96@gmail.com>
2022-08-10 12:01:35 +0300

    сделала задачу 1

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        byte a = 120;
+        short b = 324;
+        int c = 3_456_234;
+        long d = -2_456_234L;
+        float e = 3.76f;
+        double f = 4.56743289;
+        char g = 45;
+        boolean h = false;
+        System.out.println("переменная первого типа равна " +a);
+        System.out.println("переменная второго типа равна " +b);
+        System.out.println("переменная третьего типа равна " +c);
+        System.out.println("переменная четвертого типа равна " +d);
+        System.out.println("переменная пятого типа равна " +e);
+        System.out.println("переменная шестого типа равна " +f);
+        System.out.println("переменная седьмого типа равна " +g);
+        System.out.println("переменная восьмого типа равна " +h);
+    }
+}
